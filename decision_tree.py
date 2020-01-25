0b68f065273c6370d6b7bde6dd94d472149e30b8
swierczj <kub9818@gmail.com>
2020-01-26 00:46:53 +0100

    add data and labels import

--- decision_tree.py
+++ decision_tree.py
@@ -1,2 +1,15 @@
-import numpy
-# TODO: everything
+import math
+
+from digits_visualisation import data, set_count #, labels_values
+
+'''
+C4.5 tree implementation
+'''
+
+# test
+print(data[0])  # [example number 0:set_count-1][row number 0:size-1][column number 0:size-1]
+
+class C45:
+    def _init_(self, dataset, labels):
+        self.dataset = dataset
+        self.labels = labels
--- digits_visualisation.py
+++ digits_visualisation.py
@@ -2,16 +2,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 import gzip
 
-f = gzip.open('train-images-idx3-ubyte.gz', 'r')
+images = gzip.open('train-images-idx3-ubyte.gz', 'r')
+labels = gzip.open('train-labels-idx1-ubyte.gz', 'r')
 image_size = 28
-count = 5
+set_count = 5
+images_file_offset = 16  # non image information
+labels_file_offset = 8
 
-f.read(16)
-buf = f.read(image_size * image_size * count)
+images.read(images_file_offset)
+buf = images.read(image_size * image_size * set_count)
 data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
-data = data.reshape(count, image_size, image_size, 1)
+data = data.reshape(set_count, image_size, image_size, 1)
 
-for i in range(0, count):
-  image = np.asarray(data[i]).squeeze()
-  plt.imshow(image)
-  plt.show()
+for i in range(0, set_count):
+    image = np.asarray(data[i]).squeeze()
+    plt.imshow(image)
+    plt.show()
+
+labels.read(labels_file_offset)
+
+#  supposing file with labels is opened, function returns list of labels
+def get_numbers_labels(labels_set, count = set_count):
+    if count <= 0:
+        return None
+    labels_result = []
+    for j in range(0, count):
+        tmp = labels_set.read(1)
+        labels_result.append(np.frombuffer(tmp, dtype=np.uint8).astype(np.int64))
+    return labels_result
