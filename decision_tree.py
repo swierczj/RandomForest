3abb6a9a4ec331bac6d51bb443c50628cb2cda67
swierczj <kub9818@gmail.com>
2020-01-26 15:52:32 +0100

    getting classes and attributes values

--- decision_tree.py
+++ decision_tree.py
@@ -1,15 +1,50 @@
 import math
 
-from digits_visualisation import data, set_count #, labels_values
+from digits_visualisation import data, set_count, get_digits_labels, labels_f  # , labels_values
 
 '''
 C4.5 tree implementation
 '''
 
 # test
-print(data[0])  # [example number 0:set_count-1][row number 0:size-1][column number 0:size-1]
+print(len(data[0][0]))  # [example number 0:set_count-1][row number 0:size-1][column number 0:size-1]
+#test = get_digits_labels(labels_f)
+# print(test[1][1])
+#classes_t = set(test)
+# print(set(get_digits_labels(labels)))
 
 class C45:
-    def _init_(self, dataset, labels):
+    def __init__(self, dataset, labels):
         self.dataset = dataset
         self.labels = labels
+        self.examples_num = len(dataset)
+        self.attributes_num = len(dataset[0]) * len(dataset[0][0])
+        self.classes = self.get_classes()
+        self.attributes_values = self.get_attributes_values()
+
+    # improve to detect single class in subset, useful in splitting in c4.5
+    def get_classes(self):
+        result = []
+        for label in self.labels:
+            if label not in result:
+                result.append(label[0])  # label is np.array type
+        return result
+
+    # check if dataset contains only one class data
+    def is_single_class(self):
+        return len(self.classes) == 1
+
+    # improve to get atrr values in subset
+    def get_attributes_values(self):
+        result = []
+        for instance in self.dataset:
+            for row in instance:
+                for pixel in row:
+                    if pixel not in result:
+                        result.append(pixel[0])  # pixel is np.array type
+        result.sort()  # debug purposes
+        return result
+
+
+
+tree = C45(data, get_digits_labels(labels_f))
--- digits_visualisation.py
+++ digits_visualisation.py
@@ -3,9 +3,9 @@ import numpy as np
 import gzip
 
 images = gzip.open('train-images-idx3-ubyte.gz', 'r')
-labels = gzip.open('train-labels-idx1-ubyte.gz', 'r')
+labels_f = gzip.open('train-labels-idx1-ubyte.gz', 'r')
 image_size = 28
-set_count = 5
+set_count = 10
 images_file_offset = 16  # non image information
 labels_file_offset = 8
 
@@ -19,14 +19,18 @@ for i in range(0, set_count):
     plt.imshow(image)
     plt.show()
 
-labels.read(labels_file_offset)
+labels_f.read(labels_file_offset)
 
-#  supposing file with labels is opened, function returns list of labels
-def get_numbers_labels(labels_set, count = set_count):
+#  function returns list of labels
+def get_digits_labels(labels_set, count = set_count):
     if count <= 0:
         return None
+    if labels_set.closed:
+        labels_set = gzip.open('train-labels-idx1-ubyte.gz', 'r')
+        labels_set.read(labels_file_offset)
     labels_result = []
     for j in range(0, count):
         tmp = labels_set.read(1)
         labels_result.append(np.frombuffer(tmp, dtype=np.uint8).astype(np.int64))
+    labels_set.close()
     return labels_result
